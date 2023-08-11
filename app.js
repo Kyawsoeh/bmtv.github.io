02e654391152294344831878c47f997a6a690957
SPYDER <98939270+Kyawsoeh@users.noreply.github.com>
2023-08-11 17:32:12 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1453,7 +1453,7 @@ function file_video(name, encoded_name, size, poster, url, mimeType, file_id, co
 			player_js = 'https://cdn.plyr.io/' + player_config.plyr_io_version + '/plyr.polyfilled.js'
 			player_css = 'https://cdn.plyr.io/' + player_config.plyr_io_version + '/plyr.css'
 		} else if (player_config.player == "videojs") {
-			player = `<video id="vplayer" poster="${poster}" muted=true class="video-js vjs-default-skin" controls preload="auto" width="100%" height="100%" data-setup='{"fluid": true}' style="--plyr-captions-text-color: #ffffff;--plyr-captions-background: #000000;">
+			player = `<video id="vplayer" poster="${poster}" muted=false class="video-js vjs-default-skin" controls preload="auto" width="100%" height="100%" data-setup='{"fluid": true}' style="--plyr-captions-text-color: #ffffff;--plyr-captions-background: #000000;">
       <source src="${url}" type="video/mp4" />
       <source src="${url}" type="video/webm" />
       <source src="${url}" type="video/avi" />
